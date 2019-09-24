45d45eb3cc265a8faec4a0ce6b307c3422492c35
laurashub <shublaura@gmail.com>
2019-09-24 15:15:29 -0700

    skeleton functions

---
+++ MapDNABinding.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as mp
+
+def get_binding_motif_data():
+	return None
+
+def find_binding_proteins(seq, database):
+	return None
+
+def show_results(seq, binding_data):
+	return None
+
